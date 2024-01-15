aa046ad1db66a3dd2f69000379195c256fed0b4c
Arthur_Macedo <arthurmacedo01@gmail.com>
2024-01-15 18:58:06 -0300

    notify

--- src/main.c
+++ src/main.c
@@ -1,30 +1,37 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+TaskHandle_t TaskReading = NULL;
+
 void read_sensor(void *pvParameters)
 {
+  int value = 0;
   while (1)
   {
-    printf("lendo sensor...\n");
+    value++;
+    printf("lendo sensor... valor: %d\n", value);
+    if (value >= 5 && value <= 10)
+    {
+      // notificar a task sending_value
+      xTaskNotifyGive(TaskReading);
+    }
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
   vTaskDelete(NULL);
 }
 void sending_value(void *pvParameters)
 {
-  int *period = (int *)pvParameters;
   while (1)
   {
-    printf("enviando valor %d...\n", *period);
-    vTaskDelay(*period / portTICK_PERIOD_MS);
+    // aguardando a notificação ...
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    printf("enviando valor...\n");
   }
   vTaskDelete(NULL);
 }
 
-int period = 3000;
-
 void app_main(void)
 {
-  xTaskCreate(read_sensor, "Leitura de Sensor", 1024, NULL, 0, NULL);
-  xTaskCreate(sending_value, "Envio de Valor", 2048, (void *)&period, 0, NULL);
+  xTaskCreate(read_sensor, "Leitura de Sensor", 2048, NULL, 0, NULL);
+  xTaskCreate(sending_value, "Envio de Valor", 2048, NULL, 0, &TaskReading);
 }
