add63cdfcd5c96c91b6ed69d20aca21bfeda7874
Arthur_Macedo <arthurmacedo01@gmail.com>
2024-01-15 19:29:16 -0300

    queue

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
 TaskHandle_t TaskReading = NULL;
+QueueHandle_t Queue1 = NULL;
 
 void read_sensor(void *pvParameters)
 {
@@ -12,8 +14,7 @@ void read_sensor(void *pvParameters)
     printf("lendo sensor... valor: %d\n", value);
     if (value >= 5 && value <= 10)
     {
-      // notificar a task sending_value
-      xTaskNotifyGive(TaskReading);
+      xQueueSendToBack(Queue1, &value, 0);
     }
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
@@ -21,17 +22,20 @@ void read_sensor(void *pvParameters)
 }
 void sending_value(void *pvParameters)
 {
+  int value = 0;
   while (1)
   {
-    // aguardando a notificação ...
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-    printf("enviando valor...\n");
+    if (xQueueReceive(Queue1, &value, portMAX_DELAY))
+    {
+      printf("enviando valor %d...\n", value);
+    }
   }
   vTaskDelete(NULL);
 }
 
 void app_main(void)
 {
+  Queue1 = xQueueCreate(10, sizeof(int));
   xTaskCreate(read_sensor, "Leitura de Sensor", 2048, NULL, 0, NULL);
   xTaskCreate(sending_value, "Envio de Valor", 2048, NULL, 0, &TaskReading);
 }
