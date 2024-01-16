31fc157acb42c87a30e387a1cd4b9e46a2359a7c
Arthur_Macedo <arthurmacedo01@gmail.com>
2024-01-16 16:58:09 -0300

    semaphore

--- src/main.c
+++ src/main.c
@@ -1,15 +1,50 @@
 #include "freertos/FreeRTOS.h"
-#include "freertos/timers.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
 
-TimerHandle_t xTimer = NULL;
+int shared_value = 0;
+SemaphoreHandle_t myMutex = NULL;
 
-void run_function(TimerHandle_t xTimer)
+void task1(void *args)
 {
-  printf("executando função após o timer expirar\n");
+  while (1)
+  {
+    xSemaphoreTake(myMutex, portMAX_DELAY);
+    shared_value++;
+    xSemaphoreGive(myMutex);
+    printf("task 1: valor: %d\n", shared_value);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void task2(void *args)
+{
+  while (1)
+  {
+    xSemaphoreTake(myMutex, portMAX_DELAY);
+    shared_value *= 2;
+    xSemaphoreGive(myMutex);
+    printf("task 2: valor: %d\n", shared_value);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void task3(void *args)
+{
+  while (1)
+  {
+    xSemaphoreTake(myMutex, portMAX_DELAY);
+    shared_value *= 3;
+    xSemaphoreGive(myMutex);
+    printf("task 3: valor: %d\n", shared_value);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
 
 void app_main(void)
 {
-  xTimer = xTimerCreate("Temp_1", 1000 / portTICK_PERIOD_MS, pdTRUE, (void *)0, run_function);
-  xTimerStart(xTimer, portMAX_DELAY);
+  myMutex = xSemaphoreCreateMutex();
+  xTaskCreate(task1, "task1", 2048, NULL, 0, NULL);
+  xTaskCreate(task2, "task2", 2048, NULL, 0, NULL);
+  xTaskCreate(task3, "task3", 2048, NULL, 0, NULL);
 }
