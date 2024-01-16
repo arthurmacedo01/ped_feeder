9154607fb75d0b83ce34ce54214364781d4a8172
Arthur_Macedo <arthurmacedo01@gmail.com>
2024-01-16 16:25:17 -0300

    timers

--- src/main.c
+++ src/main.c
@@ -1,41 +1,15 @@
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
+#include "freertos/timers.h"
 
-TaskHandle_t TaskReading = NULL;
-QueueHandle_t Queue1 = NULL;
+TimerHandle_t xTimer = NULL;
 
-void read_sensor(void *pvParameters)
+void run_function(TimerHandle_t xTimer)
 {
-  int value = 0;
-  while (1)
-  {
-    value++;
-    printf("lendo sensor... valor: %d\n", value);
-    if (value >= 5 && value <= 10)
-    {
-      xQueueSendToBack(Queue1, &value, 0);
-    }
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-  vTaskDelete(NULL);
-}
-void sending_value(void *pvParameters)
-{
-  int value = 0;
-  while (1)
-  {
-    if (xQueueReceive(Queue1, &value, portMAX_DELAY))
-    {
-      printf("enviando valor %d...\n", value);
-    }
-  }
-  vTaskDelete(NULL);
+  printf("executando função após o timer expirar\n");
 }
 
 void app_main(void)
 {
-  Queue1 = xQueueCreate(10, sizeof(int));
-  xTaskCreate(read_sensor, "Leitura de Sensor", 2048, NULL, 0, NULL);
-  xTaskCreate(sending_value, "Envio de Valor", 2048, NULL, 0, &TaskReading);
+  xTimer = xTimerCreate("Temp_1", 1000 / portTICK_PERIOD_MS, pdTRUE, (void *)0, run_function);
+  xTimerStart(xTimer, portMAX_DELAY);
 }
